93edf386a184ef9a0ae54fefc48f1e52f06d7af2
Podjan <fauzanmohammad188@gmail.com>
2025-01-07 21:06:54 +0700

    deploy2

--- deploy-v1.py
+++ deploy-v1.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 import joblib
